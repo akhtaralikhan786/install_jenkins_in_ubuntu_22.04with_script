647aa9138987369ef5c638d4e35d47d997239779
akhtaralikhan786 <129357653+akhtaralikhan786@users.noreply.github.com>
2024-05-11 12:12:44 +0530

    Create jeinkins_install_ubuntu22.04.sh

---
+++ jeinkins_install_ubuntu22.04.sh
@@ -0,0 +1,8 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo ufw allow 8080
+sudo ufw status
